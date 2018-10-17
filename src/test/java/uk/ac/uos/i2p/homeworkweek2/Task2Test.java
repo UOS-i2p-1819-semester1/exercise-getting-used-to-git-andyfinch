e111208576ab37bdc802661a7403a2578cad01c5
Andy Finch <S193805@uos.ac.uk>
2018-10-17 15:58:03 +0100

    Add task2 temp

---
+++ src/test/java/uk/ac/uos/i2p/homeworkweek2/Task2Test.java
@@ -0,0 +1,17 @@
+package uk.ac.uos.i2p.homeworkweek2;
+
+import org.junit.jupiter.api.Test;
+
+public class Task2Test {
+
+    /*Write a short program that computes areas of different shapes where user specifies first the desired shape
+    - T for triangle, S for square, R for rectangle, and C for Circle - and program then acquires necessary dimensions from user.
+    Program should be coded in two versions, one using nested if and the other a case construct.*/
+
+    @Test
+    void name() {
+
+       /* Task2 task2 = new Task2();
+        task2.dimensionsUsingIf("S", )*/
+    }
+}
