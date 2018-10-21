380bc7137e4b382d3c13dae7552d4f3aa3b02ffb
AndyFinch <andrewfinch@mcpplc.com>
2018-10-21 15:25:18 +0100

    Task 2 complete

--- src/main/java/uk/ac/uos/i2p/homeworkweek2/Task2.java
+++ src/main/java/uk/ac/uos/i2p/homeworkweek2/Task2.java
@@ -1,12 +1,119 @@
 package uk.ac.uos.i2p.homeworkweek2;
 
+import java.io.Console;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Task2 {
 
-    public void dimensionsUsingIf(String shapeType) {
+    /*Write a short program that computes areas of different shapes where user specifies first the desired shape
+    - T for triangle, S for square, R for rectangle, and C for Circle - and program then acquires necessary dimensions from user.
+    Program should be coded in two versions, one using nested if and the other a case construct.*/
+
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        System.out.println(s);
+        Task2 task2 =new Task2();
+        //String shapeType = in.nextLine();
+        task2.getShapeArea(in,true);
+
     }
+
+    public double getShapeArea(Scanner scanner) {
+        return getShapeArea(scanner, true);
+    }
+
+    public double getShapeArea(Scanner scanner, boolean useIf) {
+
+        String shapeType = scanner.next();
+
+        if (!shapeType.equalsIgnoreCase("C")
+                && !shapeType.equalsIgnoreCase("T")
+                && !shapeType.equalsIgnoreCase("S")
+                && !shapeType.equalsIgnoreCase("R"))
+        {
+            throw new IllegalArgumentException("Shape type must C, T, S, or R");
+        }
+
+
+        double area = 0;
+
+        if ( useIf) {
+            if ("T".equalsIgnoreCase(shapeType)) {
+                System.out.println("You entered triangle, please enter the base length");
+                double base = scanner.nextDouble();
+                System.out.println("Please enter the height");
+                double height = scanner.nextDouble();
+                area = (base * height) / 2;
+                System.out.println("Triangle area is " + roundArea(area));
+            } else if ("S".equalsIgnoreCase(shapeType)) {
+                System.out.println("You entered Square, please enter the length");
+                double length = scanner.nextDouble();
+                area = (Math.pow(length, 2));
+                System.out.println("Square area is " + roundArea(area));
+            } else if ("R".equalsIgnoreCase(shapeType)) {
+                System.out.println("You entered Rectangle, please enter the length");
+                double length = scanner.nextDouble();
+                System.out.println("Please enter the height");
+                double height = scanner.nextDouble();
+                area = length * height;
+                System.out.println("Square area is " + roundArea(area));
+            } else if ("C".equalsIgnoreCase(shapeType)) {
+                System.out.println("You entered circle, please enter the radius");
+                double circleRadius = scanner.nextDouble();
+                area = Math.PI * (Math.pow(circleRadius, 2));
+                System.out.println("Circle area is " + roundArea(area));
+
+            }
+        }
+        else
+        {
+            switch(shapeType) {
+                case "T" :
+                    System.out.println("You entered triangle, please enter the base length");
+                    double base = scanner.nextDouble();
+                    System.out.println("Please enter the height");
+                    double height = scanner.nextDouble();
+                    area = (base * height) / 2;
+                    System.out.println("Triangle area is " + roundArea(area));
+                    break;
+                case "S" :
+                    System.out.println("You entered Square, please enter the length");
+                    double length = scanner.nextDouble();
+                    area = (Math.pow(length, 2));
+                    System.out.println("Square area is " + roundArea(area));
+                    break;
+                case "R" :
+                    System.out.println("You entered Rectangle, please enter the length");
+                    double rLength = scanner.nextDouble();
+                    System.out.println("Please enter the height");
+                    double rHeight = scanner.nextDouble();
+                    area = rLength * rHeight;
+                    System.out.println("Square area is " + roundArea(area));
+                    break;
+                case "C" :
+                    System.out.println("You entered circle, please enter the radius");
+                    double circleRadius = scanner.nextDouble();
+                    area = Math.PI * (Math.pow(circleRadius, 2));
+                    System.out.println("Circle area is " + roundArea(area));
+
+
+            }
+        }
+
+
+        return roundArea(area);
+
+
+    }
+
+    private double roundArea(double area)
+    {
+        return new BigDecimal(area).setScale(2, RoundingMode.HALF_UP).doubleValue();
+
+    }
+
+
+
+
 }
--- src/test/java/uk/ac/uos/i2p/homeworkweek2/Task2Test.java
+++ src/test/java/uk/ac/uos/i2p/homeworkweek2/Task2Test.java
@@ -1,6 +1,12 @@
 package uk.ac.uos.i2p.homeworkweek2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class Task2Test {
 
@@ -9,9 +15,167 @@ public class Task2Test {
     Program should be coded in two versions, one using nested if and the other a case construct.*/
 
     @Test
-    void name() {
+    void testCircleArea() {
 
         Task2 task2 = new Task2();
-        task2.dimensionsUsingIf("S");
+        Scanner scanner = new Scanner("C 10");
+        double area = task2.getShapeArea(scanner);
+        assertEquals(314.16, area);
+
+        scanner = new Scanner("C 10");
+        area = task2.getShapeArea(scanner, false);
+        assertEquals(314.16, area);
+
+    }
+
+    @Test
+    void testCircleAreaDoubleRadius() {
+
+        Task2 task2 = new Task2();
+        Scanner scanner = new Scanner("C 10.5");
+        double area = task2.getShapeArea(scanner);
+        assertEquals(346.36, area);
+        scanner = new Scanner("C 10.5");
+        area = task2.getShapeArea(scanner,false);
+        assertEquals(346.36, area);
+
+    }
+
+    @Test
+    void testCircleArea0() {
+
+        Task2 task2 = new Task2();
+        Scanner scanner = new Scanner("C 0");
+        double area = task2.getShapeArea(scanner);
+        assertEquals(0, area);
+        scanner = new Scanner("C 0");
+        area = task2.getShapeArea(scanner,false);
+        assertEquals(0, area);
+
+    }
+
+    @Test
+    void testSquareArea() {
+
+        Task2 task2 = new Task2();
+        Scanner scanner = new Scanner("S 10");
+        double area = task2.getShapeArea(scanner);
+        assertEquals(100, area);
+        scanner = new Scanner("S 10");
+        area = task2.getShapeArea(scanner,false);
+        assertEquals(100, area);
+
+    }
+
+    @Test
+    void testSquareAreaDoubleRadius() {
+
+        Task2 task2 = new Task2();
+        Scanner scanner = new Scanner("S 10.5");
+        double area = task2.getShapeArea(scanner);
+        assertEquals(110.25, area);
+
+        scanner = new Scanner("S 10.5");
+        area = task2.getShapeArea(scanner, false);
+        assertEquals(110.25, area);
+
+    }
+
+    @Test
+    void SquareArea0() {
+
+        Task2 task2 = new Task2();
+        Scanner scanner = new Scanner("S 0");
+        double area = task2.getShapeArea(scanner);
+        assertEquals(0, area);
+
+        scanner = new Scanner("S 0");
+        area = task2.getShapeArea(scanner, false);
+        assertEquals(0, area);
+
+    }
+
+    @Test
+    void testRectangleArea() {
+
+        Task2 task2 = new Task2();
+        Scanner scanner = new Scanner("R 10 5");
+        double area = task2.getShapeArea(scanner);
+        assertEquals(50, area);
+
+        scanner = new Scanner("R 10 5");
+        area = task2.getShapeArea(scanner, false);
+        assertEquals(50, area);
+
+    }
+
+    @Test
+    void testRectangleAreaDoubleRadius() {
+
+        Task2 task2 = new Task2();
+        Scanner scanner = new Scanner("R 10.5 5.5");
+        double area = task2.getShapeArea(scanner);
+        assertEquals(57.75, area);
+
+        scanner = new Scanner("R 10.5 5.5");
+        area = task2.getShapeArea(scanner, false);
+        assertEquals(57.75, area);
+
+    }
+
+    @Test
+    void testRectangleArea0() {
+
+        Task2 task2 = new Task2();
+        Scanner scanner = new Scanner("R 10 0");
+        double area = task2.getShapeArea(scanner);
+        assertEquals(0, area);
+
+        scanner = new Scanner("R 10 0");
+        area = task2.getShapeArea(scanner, false);
+        assertEquals(0, area);
+
+    }
+
+    @Test
+    void testTriangleArea() {
+
+        Task2 task2 = new Task2();
+        Scanner scanner = new Scanner("T 10 5");
+        double area = task2.getShapeArea(scanner);
+        assertEquals(25, area);
+
+        scanner = new Scanner("T 10 5");
+        area = task2.getShapeArea(scanner, false);
+        assertEquals(25, area);
+
+    }
+
+    @Test
+    void testTriangleAreaDoubleRadius() {
+
+        Task2 task2 = new Task2();
+        Scanner scanner = new Scanner("T 10.5 5.5");
+        double area = task2.getShapeArea(scanner);
+        assertEquals(28.88, area);
+
+        scanner = new Scanner("T 10.5 5.5");
+        area = task2.getShapeArea(scanner,false);
+        assertEquals(28.88, area);
+
+    }
+
+    @Test
+    void testTriangleArea0() {
+
+        Task2 task2 = new Task2();
+        Scanner scanner = new Scanner("T 10 0");
+        double area = task2.getShapeArea(scanner);
+        assertEquals(0, area);
+
+        scanner = new Scanner("T 10 0");
+        area = task2.getShapeArea(scanner,false);
+        assertEquals(0, area);
+
     }
 }
